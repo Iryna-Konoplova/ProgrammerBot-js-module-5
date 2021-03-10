b92410fd18a6460b5cf588f431124c22f6afaff9
Iryna Konoplova <a.irina773@gmail.com>
2021-03-11 01:31:31 +0200

    Решает задачи 7 - 14

---
+++ js/task-9.js
@@ -0,0 +1,17 @@
+// Выполни рефакторинг кода, заменив функцию - конструктор Car на класс с
+// методом - конструктором, принимающим объект.
+
+// function Car({ brand, model, price }) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+
+class Car {
+  constructor({ brand, model, price }) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
+}
